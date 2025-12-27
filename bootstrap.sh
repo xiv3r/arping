06e9d7fa0d54f4801bd8d64011056a1dea835bd3
Thomas Habets <thomas@habets.se>
2025-12-27 21:33:48 +0000

    workflow/OpenBSD: Add an openbsd CI github action
    Only builds, but that's not half bad.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,12 @@
 #!/bin/sh
-autoreconf -i
+
+if [ "x$(uname -s)" = "xOpenBSD" ]; then
+        # On OpenBSD, you need to go searching for the autoconf version.
+        # Sigh.
+        export AUTOCONF_VERSION=$(/usr/local/bin/autoconf-* --version | sed -n '1s/.* \([0-9.]*\)$/\1/p')
+        export AUTOMAKE_VERSION=$(/usr/local/bin/automake-* --version | sed -n '1s/.* \([0-9.]*\)$/\1/p')
+        echo "Autoconf version: ${AUTOCONF_VERSION}"
+        echo "Automake version: ${AUTOMAKE_VERSION}"
+fi
+
+exec autoreconf -i
